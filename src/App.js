bd915e38a3f434f1c7fa3577ac695fa9f5e9a6bc
msko <kms89123@gmail.com>
2022-04-17 21:21:50 +0900

    Apollo GraphQL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Route, HashRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
-import { Detail } from '../routes/Detail';
-import { Home } from '../routes/Home';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import { ApolloProvider } from '@apollo/client';
-import App from './components/App';
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import client from './apollo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
 );
